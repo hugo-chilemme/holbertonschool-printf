3281657494e2b6340a7f5d58d922ed6528dc7ce0
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-12 06:11:44 -0800

    Betty correction

--- _print_INT_MIN.c
+++ _print_INT_MIN.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-* _print_str - Entry point...
+* _print_INT_MIN - Entry point...
 * Description: 'the program desc'
 * Return: Always 0 (Success)
-* @arg: va_list parmameter
 */
 
-int _print_INT_MIN()
+int _print_INT_MIN(void)
 {
 	int m = 1;
 	int min = 214748364;
-	
-	_putchar('-');	
-	parse_int(min, min, 1, &m);	
+
+	_putchar('-');
+	parse_int(min, min, 1, &m);
 	_putchar('8');
 
 	return (11);
