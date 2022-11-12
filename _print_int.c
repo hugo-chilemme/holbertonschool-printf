43e3f89d5f88c0cf08343b26536c949dd9c425ed
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-12 02:27:41 -0800

    Long int mode

--- _print_int.c
+++ _print_int.c
@@ -12,8 +12,7 @@ int _print_int(va_list arg)
 	int a = 0;
 	int val = va_arg(arg, int);
 	if (INT_MIN == val) return _print_INT_MIN();	
-	if (INT_MAX >= val) a+=1; /** long int mode */
-
+	if (val >= INT_MAX || INT_MIN <= val) a+=1; /** long int mode */
 	if (val < 0)
 		a += _putchar('-');
 
