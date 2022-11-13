b311ee50f72f80528a6bbcdbfd170c1cec8f7b17
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-13 05:12:50 -0800

    New binary system

---
+++ _print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _print_binary - Entry point...
+* Description: 'the program desc'
+* Return: Always 0 (Success)
+* @arg: va_list parmameter
+*/
+int _print_binary(va_list arg)
+{
+	int bin = va_arg(arg, int);
+	int binRes = bin;
+	int binMax = 1;
+	int binIndex = 0;
+
+	while (binMax < bin)
+		binMax *= 2;
+
+	while (binMax > 0)
+	{
+		binIndex += 1;
+		if (binMax <= binRes)
+		{
+			_putchar('1');
+			binRes -= binMax;
+		} 
+		else
+			_putchar('0');
+		binMax /= 2;
+	}	
+	return (binIndex);
+}
